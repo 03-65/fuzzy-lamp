5611d60fb15c1fda5ff3f0d7380dc773c0bbbbc3
Pipre <75277403+03-65@users.noreply.github.com>
2021-07-30 08:26:27 -0500

    Add files via upload

---
+++ embed-gist-inline.tsx
@@ -0,0 +1,66 @@
+import React from 'dom-chef';
+import domify from 'doma';
+import select from 'select-dom';
+import * as pageDetect from 'github-url-detection';
+
+import features from '.';
+
+const isGist = (link: HTMLAnchorElement): boolean =>
+	!link.pathname.includes('.') && // Exclude links to embed files
+	(
+		(link.hostname.startsWith('gist.') && link.pathname.includes('/', 1)) || // Exclude user links
+		link.pathname.startsWith('gist/')
+	);
+
+const isOnlyChild = (link: HTMLAnchorElement): boolean => link.textContent!.trim() === link.parentNode!.textContent!.trim();
+
+async function embedGist(link: HTMLAnchorElement): Promise<void> {
+	const info = <em> (loading)</em>;
+	link.after(info);
+
+	try {
+		// Fetch via background.js due to CORB policies
+		const gistData = await browser.runtime.sendMessage({fetchJSON: `${link.href}.json`});
+		if (gistData.div.length > 10000) {
+			info.textContent = ' (too large to embed)';
+			return;
+		}
+
+		const fileCount: number = gistData.files.length;
+		if (fileCount > 1) {
+			info.textContent = ` (${fileCount} files)`;
+		} else {
+			const container = <div/>;
+			container.attachShadow({mode: 'open'}).append(
+				<style>{`
+					.gist .gist-data {
+						max-height: 16em;
+						overflow-y: auto;
+					}
+				`}
+				</style>,
+				<link rel="stylesheet" href={gistData.stylesheet}/>,
+				domify.one(gistData.div)!
+			);
+			link.parentElement!.after(container);
+			info.remove();
+		}
+	} catch {
+		info.remove();
+	}
+}
+
+function init(): void {
+	for (const link of select.all('.js-comment-body p a:only-child')) {
+		if (isGist(link) && isOnlyChild(link)) {
+			void embedGist(link);
+		}
+	}
+}
+
+void features.add(__filebasename, {
+	include: [
+		pageDetect.hasComments
+	],
+	init
+});
